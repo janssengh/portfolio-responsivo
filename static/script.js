4009ef0c09313a3f043fe2c1fa53695e1563311b
Roeland <roeland.e.janssen@gmail.com>
2024-01-22 17:21:10 -0300

    seção sobre e habilidades

--- static/script.js
+++ static/script.js
@@ -9,3 +9,17 @@ menuMobile.addEventListener('click', () => {
     : menuMobile.classList.replace("bi-x", "bi-list");
     body.classList.toggle("menu-nav-active");
 });
+
+/* Fecha o menu quando clicar em algum item e muda icone para list */
+const NavItem = document.querySelectorAll('.nav-item')
+
+/* console.log(Navitem)*/
+
+NavItem.forEach(item => {
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")){
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    })
+})
